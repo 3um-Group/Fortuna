9f5bf2d350312e39d8273bf2b4cf2ada667b3fa5
Dwight Spencer (denzuko@mastodon.social) <dspencer@computekindustries.com>
2023-09-15 11:32:15 +0000

    Pending changes exported from your codespace

--- src/components/NewsApiView/Articles.test.tsx
+++ src/components/NewsApiView/Articles.test.tsx
@@ -1,11 +1,17 @@
-import { renderHook } from '@testing-library/react-hooks'
+import 'module-alias/register';
 
-import * as React from "react";
-import * as ReactDOMClient from "react-dom/client"
-import { Client, Provider, useQuery } from "urql"
+//import { Client, Provider, useQuery } from "urql"
+
+//import {cleanup, fireEvent, render, waitFoFor } '@testing-library/react';
+//import { renderHook, RenderOptions, RenderResult } from '@testing-library/react-hooks'
+
+import {cleanup, render, waitFor } from '@testing-library/react';
+import { Client, Provider } from "urql";
 
 import NewsApiView, {ArticleQuery, ArticleQuerySchema} from "./NewsApiView"
 import NewsClient from '../../api/NewsApiClient'
+
+afterEach(cleanup);
 
 const NewsApi:Client = NewsClient({
   region: 'us-east-1-shared-usea1-02', 
@@ -14,13 +20,13 @@ const NewsApi:Client = NewsClient({
   environment: 'master'
 });
 
+/*
 it("returns more than one article from provider without error", () => {
-  const div = document.createElement("div");
-  const root = ReactDOMClient.createRoot(div);
-
-  root.render(<Provider value={NewsApi}><NewsApiView/></Provider>)
+  const {queryByCardText, getByCardText} = render(
+    <Provider value={NewsApi}><NewsApiView/></Provider>
+  );
   
-  const [result] = renderHook(() => useQuery<ArticleQuery>({ query: ArticleQuerySchema }));
+  const {result} = renderHook(() => useQuery<ArticleQuery>({ query: ArticleQuerySchema }));
   const { data, fetching, error } = result;
 
   expect(fetching).toBeFalsy();
@@ -28,12 +34,17 @@ it("returns more than one article from provider without error", () => {
   expect(data).toHaveProperty("Article");
   expect(data?.Articles).toBeGreaterThan(0);
 
-  root.unmount();
-});
+  expect(queryByCardText(/No News/i)).toBeTruthy();
+  fireEvent.click(getByCardText(/No News/i));
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  const root = ReactDOMClient.createRoot(div);
-  root.render(<Provider value={NewsApi}><NewsApiView /></Provider>);
-  root.unmount();
+});
+*/
+
+it("renders without crashing", async() => {
+  const { queryByTestId, asFragment } = render(<Provider value={NewsApi}><NewsApiView /></Provider>);
+
+  await waitFor(() => {
+    expect(asFragment()).toMatchSnapshot();
+    //expect(queryByTestId('Articles').childNodes.length).toBeGreaterThan(0)
+  });
 });
---
+++ src/components/WalletView/WalletView.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta } from '@storybook/react';
+import WalletView from '.';
+
+const meta = {
+  title: 'WalletView/UX/Compoent',
+  component: WalletView,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  }
+} satisfies Meta<typeof WalletView>;
+
+export default meta;
