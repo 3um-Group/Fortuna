2fdadcd6e71042677ba92eb8fd9ac115669ae1ef
Dwight Spencer (denzuko@mastodon.social) <dspencer@computekindustries.com>
2023-10-13 17:05:58 +0000

    (bugfix) Resolve vercel release issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
 import * as React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Client, Provider } from "urql";
+import { Provider } from "urql";
 
+/*
 import NewsClient from "./api/NewsApiClient";
 import NewsApiView from "./components/NewsApiView/NewsApiView";
-
 const NewsApi:Client = NewsClient({
   region: 'us-east-1-shared-usea1-02', 
   version: 2,
   key: 'cllcilper2zc701tc3wu0652t',
   environment: 'master'
 });
+*/
+
+type routeItem = {
+  path: string,
+  errorElement: JSX.Element,
+  element: JSX.Element,
+}
+
+function routeFactory(path:string, errorElement: JSX.Element, element:JSX.Element): routeItem {
+  return {path, errorElement, element}
+}
+
+/*
+    routeFactory("/NewsView",
+(<div><h1>Error loading route...</h1></div>),
+(<Provider value={NewsApi}><NewsApiView /></Provider>)),
+*/
 
 function App(): JSX.Element {
   const router = createBrowserRouter([
-    {
-    path: "/NewsView",
-    errorElement: (
-      <div>
-        <h1> Error loading route...</h1>
-      </div>
-    ),
-    element: (<Provider value={NewsApi}><NewsApiView/></Provider>
-    )
-    }, {
-      path: "/",
-      errorElement: (
-        <div>
-          <h1> Error loading route...</h1>
-        </div>
-      ),
-      element: (
-       <><p>Loading main view</p></>
-      ),
-    },
+    routeFactory("/",
+          (<div><h1>Error loading route...</h1></div>),
+          (<><p>Loading main view</p></>))
   ]);
   return <RouterProvider router={router} />;
 }
