f41889b2fc3636c374073a32502e6a472237ad28
Anjali-3UM <anjali.s@3umgroup.com>
2024-07-25 22:38:26 -0500

    Add initial images with carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Header from './components/Header/Header';
 import { LoginButton, LogoutButton } from './components/AuthElement';
+import EmblaCarousel from './components/ProductDetailsCarousel/EmblaCarousel'
+import { EmblaOptionsType } from 'embla-carousel'
 
+import './components/ProductDetailsCarousel/css/base.css'
+import './components/ProductDetailsCarousel/css/sandbox.css'
+import './components/ProductDetailsCarousel/css/embla.css'
+
+const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true, slidesToScroll: "auto" }
+const SLIDE_COUNT = 5
+// const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys()).map(i => `/properties/property${i + 1}.jpg`);
+
+console.log(SLIDES)
+console.log(OPTIONS)
 const App: React.FC = () => {
   const { isAuthenticated } = useAuth0();
   console.log("Authorized ?", isAuthenticated)
@@ -17,6 +31,9 @@ const App: React.FC = () => {
         )}
       </Header>
       {isAuthenticated && <p>Welcome! You are logged in.</p>}
+      <div className="theme-dark">
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </div>
     </>
   );
 };
