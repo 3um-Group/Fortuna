7b3048d0a4e9ba816b6b845d10ab6231e1a847da
ganesh-3um <ganesh.s@3umgroup.com>
2024-08-09 11:10:55 -0400

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Header from './components/Header/Header';
-import { LoginButton, LogoutButton } from './components/AuthElement';
-
+import { Header } from '@3um-Group/atomic-sdk'
+import { useThemeContext } from './context/ThemeContext';
 const App: React.FC = () => {
-  const { isAuthenticated } = useAuth0();
-  console.log("Authorized ?", isAuthenticated)
+
+//   const getLogoSrc = () => {
+//     return theme === 'dark' ? '/assets/3UM-white-logo.png' : '/assets/3UM-dark-logo.png';
+// };
+  const { theme } = useThemeContext();
+  const useAuth = () => {
+    const auth0 = useAuth0();
+    return {
+      isAuthenticated: auth0.isAuthenticated,
+      loginWithRedirect: auth0.loginWithRedirect,
+      logout: auth0.logout,
+    };
+  };
+
+  // console.log("yey",useAuth().loginWithRedirect())
 
   return (
-    <>
-      <Header theme="dark">
-        {isAuthenticated ? (
-          <LogoutButton theme="dark" />
-        ) : (
-          <LoginButton theme="dark" />
-        )}
-      </Header>
-      {isAuthenticated && <p>Welcome! You are logged in.</p>}
-    </>
+    <div className='mx-6 mt-2'>
+      <Header
+        logoProps={{
+          alt: 'Company Logo',
+          customLightSrc:'/assets/3UM-dark-logo.png',
+          customDarkSrc:'/assets/3UM-white-logo.png',
+          height: 50,
+          theme: 'light',
+          width: 50,
+        }}
+        theme= {theme}
+        useAuth={useAuth}
+        showNavItems
+      />
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/@3um-Group/atomic-sdk/**/*.{js,jsx,ts,tsx}"
     //"node_modules/daisyui/**/*.{js,jsx,ts,tsx}"
   ],
   daisyui: {
-    themes: ["cupcake", "dark", "cmyk"],
+    themes: ["light", "dark",  "cupcake", "cmyk"], // Remove "dark" from this array
   },
   theme: {
     extend: {},
