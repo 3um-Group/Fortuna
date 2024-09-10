1fe58473405bdcebe9e2ef8808f48ab3ebc74ae3
Bill Tallitsch <william.t@3umgroup.com>
2024-09-10 14:06:02 -0400

    Sidebar and homeview layout updates

--- src/App.tsx
+++ src/App.tsx
@@ -9,178 +9,195 @@ const App: React.FC = () => {
 // };
   // const { theme } = useThemeContext();
 
-  var properties = [
-    {
-      id: 1,
-      location: "Atlanta, GA 30306",
-      price: "$2,500,000",
-      imageSrc:"https://media-cdn.trulia-local.com/neighborhood-media-service-prod/ma/boston/hyde-park/1269-ma_bos_hyde_park_154775_23_500x_cfit.jpg",
-      imageAlt:"Beautiful Home",
-      description:"1941 Lenox Rd NE",
-      badges:[{
-        label: 'Cash Only'
-      },{
-        label: 'Reported Vacant'
-      },
-      {
-        label: 'No Buyers Premium'
-      },
-      {
-        label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 2,
-      location: "Los Angeles, CA",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/193b33e8d7dfa790389c1b76f89fa9ce-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[{
-        label: 'Cash Only'
-      },
-      {
-        label: 'No Buyers Premium'
-      },
-      {
-        label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 3,
-      location: "Miami, FL",
-      price: "$3,200,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/0192d9de7bbdd118f04606b8cdda1bfe-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 4,
-      location: "Wake Forest, NC",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/a5271aa82e1dcaa09585bdeecaaeb48c-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 5,
-      location: "Los Angeles, CA",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/a3a57cddb5b77a3419c58a274bc78bac-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 6,
-      location: "Los Angeles, CA",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/375b3d96fa5dc5b3300fe64a35fa46c3-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[{
-        label: 'Cash Only'
-      },
-      {
-        label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 7,
-      location: "Los Angeles, CA",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/f12c332aec7b9eb832bf6435a6f4cb55-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[{
-        label: 'Cash Only'
-      },
-      {
-        label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 8,
-      location: "Los Angeles, CA",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/1f0675b4218cef35f9e9f062b947fff2-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[{ label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 9,
-      location: "Raleigh, NC",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/228e3a80c0a443392135daaf714a7041-full.webp",
-      imageAlt:"Beautiful Home",
-      description:'',
-      badges:[{ label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    },
-    {
-      id: 10,
-      location: "Raleigh, NC",
-      price: "$2,500,000",
-      imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/680e1d487784cf004c5e9bbe2f3bb9ed-full.webp",
-      imageAlt:"Beautiful Home",
-      description:"",
-      badges:[{ label: 'Open House' }],
-      beds:3,
-      baths:3,
-      sqft:3567,
-    }
-  ];
+  // interface Props {
+  //   children: React.ReactElement
+  // }
 
-  var PropertyList: React.FC = () => {
+  function sidebarItems() {
     return (
       <>
-        {properties.map(property => (
-            <PropertyListCard
-              key={property.id}
-              badgeColors={{
-                'Cash Only': 'bg-transparent text-red-500 border border-red-500',
-                'No Buyers Premium': 'bg-transparent text-blue-500 border border-blue-500',
-                'Open House': 'bg-transparent text-orange-500 border border-orange-500',
-                'Reported Vacant': 'bg-transparent text-black border border-black'
-              }}
-              imageSrc={property.imageSrc}
-              imageAlt={property.imageAlt}
-              price={property.price}
-              description={property.description}
-              location={property.location}
-              badges={property.badges}
-              beds={property.beds}
-              baths={property.baths}
-              sqft={property.sqft}
-              onRegister={function (): void {
-                throw new Error('Function not implemented.');
-              }}
-            />
+        <li></li>
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+        <li></li>
+        <li><a className='btn btn-primary'>Wallet</a></li>
+      </>
+    );
+  }
+  
+  function PropertyList() {
+
+    const properties = [
+      {
+        id: 1,
+        location: "Atlanta, GA 30306",
+        price: "$2,500,000",
+        imageSrc:"https://media-cdn.trulia-local.com/neighborhood-media-service-prod/ma/boston/hyde-park/1269-ma_bos_hyde_park_154775_23_500x_cfit.jpg",
+        imageAlt:"Beautiful Home",
+        description:"1941 Lenox Rd NE",
+        badges:[{
+          label: 'Cash Only'
+        },{
+          label: 'Reported Vacant'
+        },
+        {
+          label: 'No Buyers Premium'
+        },
+        {
+          label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 2,
+        location: "Los Angeles, CA",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/193b33e8d7dfa790389c1b76f89fa9ce-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[{
+          label: 'Cash Only'
+        },
+        {
+          label: 'No Buyers Premium'
+        },
+        {
+          label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 3,
+        location: "Miami, FL",
+        price: "$3,200,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/0192d9de7bbdd118f04606b8cdda1bfe-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 4,
+        location: "Wake Forest, NC",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/a5271aa82e1dcaa09585bdeecaaeb48c-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 5,
+        location: "Los Angeles, CA",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/a3a57cddb5b77a3419c58a274bc78bac-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 6,
+        location: "Los Angeles, CA",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/375b3d96fa5dc5b3300fe64a35fa46c3-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[{
+          label: 'Cash Only'
+        },
+        {
+          label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 7,
+        location: "Los Angeles, CA",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/f12c332aec7b9eb832bf6435a6f4cb55-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[{
+          label: 'Cash Only'
+        },
+        {
+          label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 8,
+        location: "Los Angeles, CA",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/1f0675b4218cef35f9e9f062b947fff2-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[{ label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 9,
+        location: "Raleigh, NC",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/228e3a80c0a443392135daaf714a7041-full.webp",
+        imageAlt:"Beautiful Home",
+        description:'',
+        badges:[{ label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      },
+      {
+        id: 10,
+        location: "Raleigh, NC",
+        price: "$2,500,000",
+        imageSrc:"https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/680e1d487784cf004c5e9bbe2f3bb9ed-full.webp",
+        imageAlt:"Beautiful Home",
+        description:"",
+        badges:[{ label: 'Open House' }],
+        beds:3,
+        baths:3,
+        sqft:3567,
+      }
+    ];
+
+    return (
+      <>
+        {properties.map((property) => (
+          <PropertyListCard
+            key={property.id} // Adding a unique 'key' prop to avoid React warnings
+            badgeColors={{
+              'Cash Only': 'bg-transparent text-red-500 border border-red-500',
+              'No Buyers Premium': 'bg-transparent text-blue-500 border border-blue-500',
+              'Open House': 'bg-transparent text-orange-500 border border-orange-500',
+              'Reported Vacant': 'bg-transparent text-black border border-black'
+            }}
+            imageSrc={property.imageSrc}
+            imageAlt={property.imageAlt}
+            price={property.price}
+            description={property.description}
+            location={property.location}
+            badges={property.badges}
+            beds={property.beds}
+            baths={property.baths}
+            sqft={property.sqft}
+            onRegister={() => {
+              console.log("Register function not implemented");
+            }}
+          />
         ))}
       </>
     );
@@ -210,14 +227,20 @@ const App: React.FC = () => {
         useAuth={useAuth}
         showNavItems
       />
-      <div>
-        <SearchBar data-test-id="search-bar"
-          onChange={(event) => { console.log(event); }}
-          onSearch={() => {}}
-          placeholder="Enter to search"
-          value=""
-        />
-        <Sidebar children={'test child'} />
+      <div className="flex flex-row">
+        <div className="basis-3/4">
+          <SearchBar data-test-id="search-bar"
+            onChange={(event) => { console.log(event); }}
+            onSearch={() => {}}
+            placeholder="Enter to search"
+            value=""
+          />
+        </div>
+        <div className="basis-1/4">
+          <Sidebar children={sidebarItems()} />
+        </div>
+      </div>
+      <div className="viewContainer">
         <PropertyList />
       </div>
     </div>
