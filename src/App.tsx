8c1c3220463e5da713b717cefa2833a6875a958b
Bill Tallitsch <william.t@3umgroup.com>
2024-09-23 17:30:45 -0400

    Removed footer added property carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
-import { Header, SearchBar, PropertyListCard, NewsCard, Footer } from '@3um-group/atomic-sdk'; 
+import { Header, PropertyListCard, NewsCard } from '@3um-group/atomic-sdk'; 
 import AuthButton from './components/AuthButton';
 import { Routes, Route, Link } from 'react-router-dom';
 import Wallet from './pages/Wallet';
 import PropertyDetails from './pages/PropertyDetails';
 import properties from './data/properties';
+import PropertyCarousel from 'src/components/PropertyDetails/PropertyCarousel';
 
 const App: React.FC = () => {
   const [newsArticles, setNewsArticles] = useState<any[]>([]); 
@@ -36,73 +37,69 @@ const App: React.FC = () => {
         <li key="messages"><a>Messages</a></li>
         <li key="change-theme"><a>Change Theme</a></li>
         <li key="connect-wallet"><a className="btn btn-primary">Connect Wallet</a></li>
+        <li><AuthButton /></li>
+        {/* TODO: Login btns here */}
       </>
     );
   }
 
   function PropertyList() {
     return (
-      <div className="flex flex-col md:flex-row gap-6 justify-center w-full mb-7"> 
-        <div className="flex-1 w-full max-w-3xl flex flex-col gap-6"> 
-          <SearchBar
-            data-test-id="search-bar"
-            onChange={(event) => { console.log(event); }}
-            onSearch={() => {}}
-            placeholder="Enter to search"
-            value=""
-            className="w-full"
-          />
-          <AuthButton />
-          {properties.map((property) => (
-            <Link to={`/property-details/${property.id}`} key={property.id}>
-              <PropertyListCard
-                key={property.id}
-                badgeColors={{
-                  'Cash Only': 'bg-transparent text-red-500 border border-red-500',
-                  'No Buyers Premium': 'bg-transparent text-blue-500 border border-blue-500',
-                  'Open House': 'bg-transparent text-orange-500 border border-orange-500',
-                  'Reported Vacant': 'bg-transparent text-black border border-black'
-                }}
-                imageSrc={property.imageSrc}
-                imageAlt={property.imageAlt}
-                price={property.price}
-                description={property.description}
-                location={property.location}
-                badges={property.badges}
-                beds={property.beds}
-                baths={property.baths}
-                sqft={property.sqft}
-                className="w-full"
-                onRegister={() => {
-                  console.log("Register function not implemented");
-                }}
-              />
-            </Link>
-          ))}
-        </div>
+      <>
+        {/* <div className='h-500 w-full overflow-x-auto'> */}
+          <PropertyCarousel />
+        {/* </div> */}
+        <div className="flex flex-col md:flex-row gap-6 justify-center w-full mb-7">
+          <div className="flex-1 w-full max-w-3xl flex flex-col gap-6">
+            {properties.map((property) => (
+              <Link to={`/property-details/${property.id}`} key={property.id}>
+                <PropertyListCard
+                  key={property.id}
+                  badgeColors={{
+                    'Cash Only': 'bg-transparent text-red-500 border border-red-500',
+                    'No Buyers Premium': 'bg-transparent text-blue-500 border border-blue-500',
+                    'Open House': 'bg-transparent text-orange-500 border border-orange-500',
+                    'Reported Vacant': 'bg-transparent text-black border border-black'
+                  }}
+                  imageSrc={property.imageSrc}
+                  imageAlt={property.imageAlt}
+                  price={property.price}
+                  description={property.description}
+                  location={property.location}
+                  badges={property.badges}
+                  beds={property.beds}
+                  baths={property.baths}
+                  sqft={property.sqft}
+                  className="w-full"
+                  onRegister={() => {
+                    console.log("Register function not implemented");
+                  } } />
+              </Link>
+            ))}
+          </div>
 
-        <div className="w-full md:w-1/4 flex flex-col gap-6"> 
-          {loading ? (
-            <p>Loading news...</p> 
-          ) : (
-            newsArticles.slice(0, 4).map((article, index) => (
-              <NewsCard
-                key={index}
-                imageSrc={article.urlToImage || 'https://via.placeholder.com/150'}
-                title={article.title}
-                description={article.description}
-                date={new Date(article.publishedAt).toLocaleDateString('en-GB', {
-                  day: 'numeric',
-                  month: 'long',
-                  year: 'numeric',
-                })}
-                linkUrl={article.url}
-                className="w-full"
-              />
-            ))
-          )}
+          <div className="w-full md:w-1/4 flex flex-col gap-6">
+            {loading ? (
+              <p>Loading news...</p>
+            ) : (
+              newsArticles.slice(0, 4).map((article, index) => (
+                <NewsCard
+                  key={index}
+                  imageSrc={article.urlToImage || 'https://via.placeholder.com/150'}
+                  title={article.title}
+                  description={article.description}
+                  date={new Date(article.publishedAt).toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric',
+                  })}
+                  linkUrl={article.url}
+                  className="w-full" />
+              ))
+            )}
+          </div>
         </div>
-      </div>
+      </>
     );
   }
 
@@ -131,7 +128,7 @@ const App: React.FC = () => {
           }}
           useAuth={useAuth}
           sidebarProps={{
-            children: sidebarItems(), // Pass sidebarItems as children
+            children: sidebarItems()
           }}
         />
 
@@ -145,25 +142,6 @@ const App: React.FC = () => {
           </div>
         </div>
 
-        <Footer
-          logoSrc="/assets/3UM-dark-logo.png" 
-          footerSections={[
-            {
-              name: 'Company',
-              links: [
-                { title: 'About Us', href: '/about' },
-                { title: 'Careers', href: '/careers' },
-              ],
-            },
-            {
-              name: 'Help',
-              links: [
-                { title: 'Contact Us', href: '/contact' },
-                { title: 'Support', href: '/support' },
-              ],
-            },
-          ]}
-        />
       </div>
     </Auth0Provider>
   );
