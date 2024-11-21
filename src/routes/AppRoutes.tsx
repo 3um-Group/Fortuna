bb4abca8076a5b450f3b7bc64c7b6f137441666f
Bill Tallitsch <william.t@3umgroup.com>
2024-11-21 11:28:47 -0500

    Hotfixes routes again

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAuth0, Auth0ContextInterface } from '@auth0/auth0-react';
+import React from 'react';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Elements } from '@stripe/react-stripe-js'; 
+import { loadStripe } from '@stripe/stripe-js'; 
+
 import Wallet from '../pages/Wallet';
 import PropertyDetails from '../pages/PropertyDetails';
 import UserProfile from '../pages/UserProfile';
@@ -13,8 +16,6 @@ import Settings from '../components/UserProfile/Settings';
 import HelpCenter from '../components/UserProfile/HelpCenter';
 import ManageCards from '../components/UserProfile/ManageCards';
 import TransactionHistory from '../components/UserProfile/TransactionHistory';
-import { Elements } from '@stripe/react-stripe-js'; 
-import { loadStripe } from '@stripe/stripe-js'; 
 import ContractPage from '../pages/ContractPage';
 import NewsFeed from '../pages/NewsFeed';
 import Article from '../pages/Article';
@@ -22,69 +23,68 @@ import Signup from '../pages/Signup';
 
 const stripePromise = loadStripe('your-publishable-key-here');
 
-const AppRoutes: React.FC = () => {
-    const { loginWithRedirect, logout } = useAuth0();
-    return (
-        <Routes>
-
-            <Route path="/" element={<Dashboard />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-            
-            <Route path="/login" element={<LoginPage loginWithRedirect={loginWithRedirect} />} />
-            <Route path="/logout" element={<LogoutPage logout={logout} />} />
-            <Route path="/signup" element={<Signup />} />
-
-            <Route path="/wallet" element={<Wallet />} />
-            <Route path="/property-details/:id" element={<PropertyDetails />} />
-            <Route path="/news" element={<NewsFeed numberOfArticles={10} />} />
-            <Route path="/article" element={<Article />} />
-            <Route path="/add-wallet" element={<AddWallet />} />
-            <Route path="/upload" element={<Upload />} />
-            <Route path="/contract" element={<ContractPage />} />
-
-            <Route path="/profile" element={<ProfileMenu />} />
-                <Route path="profile/user" element={<UserProfile />} />
-                <Route path="profile/my-account" element={<MyAccount />} />
-                <Route path="profile/settings" element={<Settings />} />
-                <Route path="profile/help-center" element={<HelpCenter />} />
-                <Route path="profile/transaction-history" element={<TransactionHistory />} />
-                <Route
-                    path="profile/card-management"
-                    element={
-                        <Elements stripe={stripePromise}>
-                            <ManageCards />
-                        </Elements>
-                    }
-                />
-        </Routes>
-    );
-};
-
-interface LoginPageProps {
-    loginWithRedirect: Auth0ContextInterface['loginWithRedirect'];
-}
-
-interface LogoutPageProps {
-    logout: Auth0ContextInterface['logout'];
-}
-
-const LoginPage: React.FC<LoginPageProps> = ({ loginWithRedirect }) => {
-    useEffect(() => {
-        loginWithRedirect();
+const LoginPage = () => {
+    const { loginWithRedirect } = useAuth0();
+    
+    React.useEffect(() => {
+      loginWithRedirect();
     }, [loginWithRedirect]);
-
+  
     return <div>Redirecting to login...</div>;
 };
+  
+const LogoutPage = () => {
+    const { logout } = useAuth0();
 
-const LogoutPage: React.FC<LogoutPageProps> = ({ logout }) => {
-    useEffect(() => {
-        logout({
-            logoutParams: { returnTo: window.location.origin },
-        });
+    React.useEffect(() => {
+        logout({ logoutParams: { returnTo: window.location.origin } });
     }, [logout]);
 
     return <div>Redirecting to logout...</div>;
 };
 
+const ProfileLayout = () => (
+    <>
+        <ProfileMenu />
+        <Outlet />
+    </>
+);
+
+const AppRoutes: React.FC = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/property-details/:id" element={<PropertyDetails />} />
+        <Route path="/news" element={<NewsFeed numberOfArticles={10} />} />
+        <Route path="/article" element={<Article />} />
+        <Route path="/add-wallet" element={<AddWallet />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/contract" element={<ContractPage />} />
+  
+        <Route path="/profile" element={<ProfileLayout />}>
+          <Route index element={<UserProfile />} />
+          <Route path="user" element={<UserProfile />} />
+          <Route path="my-account" element={<MyAccount />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="help-center" element={<HelpCenter />} />
+          <Route path="transaction-history" element={<TransactionHistory />} />
+          <Route 
+            path="card-management" 
+            element={
+              <Elements stripe={stripePromise}>
+                <ManageCards />
+              </Elements>
+            } 
+          />
+        </Route>
+  
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    );
+  };
 
 export default AppRoutes;
